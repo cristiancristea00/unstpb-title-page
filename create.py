01b78f02c48e695a4e51bd56c71487935a1cb957
Cristian Cristea <cristiancristea00@gmail.com>
2020-09-15 23:58:57 +0300

    Strutured the script in 2 classes

--- create.py
+++ create.py
@@ -1,90 +1,137 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser, RawTextHelpFormatter
-from subprocess import run
+from subprocess import CompletedProcess, run
+from typing import Dict, Tuple
 from os.path import join
 
-description: str = 'Script that generates the PDF file based on the information provided by the user.'
-additional_information: str = 'The options available for the faculty parameter are:\n' \
-                              'FAIMA - Faculty of Entrepreneurship, Business Engineering and Management\n' \
-                              'FAC - Faculty of Automatic Control and Computers\n' \
-                              'FCASM - Faculty of Applied Chemistry and Materials Science\n' \
-                              'FETTI - Faculty of Electronics, Telecommunications and Information Technology\n' \
-                              'FE - Faculty of Power Engineering\n' \
-                              'FISB - Faculty of Biotechnical Systems Engineering\n' \
-                              'FIA - Faculty of Aerospace Engineering\n' \
-                              'FIE - Faculty of Electrical Engineering\n' \
-                              'FIIR - Faculty of Industrial Engineering and Robotics\n' \
-                              'FILS - Faculty of Engineering in Foreign Languages\n' \
-                              'FIMM - Faculty of Mechanical Engineering and Mechatronics\n' \
-                              'FIM - Faculty of Medical Engineering\n' \
-                              'FSIM - Faculty of Material Science and Engineering\n' \
-                              'FSA - Faculty of Applied Sciences\n' \
-                              'FT - Faculty of Transports\n'
 
-argument_parser = ArgumentParser(allow_abbrev = False, description = description, epilog = additional_information,
-                                 formatter_class = RawTextHelpFormatter)
-argument_parser.add_argument('-ni', '--non-interactive', action = 'store_false',
-                             help = 'Used if you want to pass the details in an non-interactive manner')
-argument_parser.add_argument('-l', '--language', choices = ['RO', 'EN', 'FR', 'DE'], metavar = '', type = str,
-                             help = 'One of the four languages: RO/EN and FR/DE just for FILS students')
-argument_parser.add_argument('-f', '--faculty', metavar = '', type = str,
-                             help = 'The faculty at which you are a student')
-argument_parser.add_argument('-t', '--type', metavar = '', type = str,
-                             help = 'The document type (e.g. Project/Lab work/...)')
-argument_parser.add_argument('-s', '--subject', metavar = '', type = str, help = 'The university subject in question')
-argument_parser.add_argument('-ti', '--title', metavar = '', type = str, help = 'The title of the document')
-argument_parser.add_argument('-fn', '--first-name', metavar = '', type = str, help = 'Your first name')
-argument_parser.add_argument('-ln', '--last-name', metavar = '', type = str, help = 'Your last name')
-argument_parser.add_argument('-g', '--group', metavar = '', type = str, help = 'The group you belong to')
-arguments = vars(argument_parser.parse_args())
+class OptionsGetter:
+    def __init__(self):
+        self.arguments: Dict[str] = {}
 
-program: str = R'xelatex'
-interaction: str = R'--interaction=scrollmode'
-job_name: str = R'--jobname={}'
+        self.description: str = 'Script that generates the PDF file based on the information provided by the user.'
+        self.additional_information: str = 'The options available for the faculty parameter are:\n' \
+                                           'FAIMA - Faculty of Entrepreneurship, Business Engineering and Management\n' \
+                                           'FAC - Faculty of Automatic Control and Computers\n' \
+                                           'FCASM - Faculty of Applied Chemistry and Materials Science\n' \
+                                           'FETTI - Faculty of Electronics, Telecommunications and Information Technology\n' \
+                                           'FE - Faculty of Power Engineering\n' \
+                                           'FISB - Faculty of Biotechnical Systems Engineering\n' \
+                                           'FIA - Faculty of Aerospace Engineering\n' \
+                                           'FIE - Faculty of Electrical Engineering\n' \
+                                           'FIIR - Faculty of Industrial Engineering and Robotics\n' \
+                                           'FILS - Faculty of Engineering in Foreign Languages\n' \
+                                           'FIMM - Faculty of Mechanical Engineering and Mechatronics\n' \
+                                           'FIM - Faculty of Medical Engineering\n' \
+                                           'FSIM - Faculty of Material Science and Engineering\n' \
+                                           'FSA - Faculty of Applied Sciences\n' \
+                                           'FT - Faculty of Transports\n'
 
-delim: str = '"'
-document_type: str = R'\newcommand{{\docType}}{{{}}}'
-subject: str = R'\newcommand{{\subject}}{{{}}}'
-title: str = R'\renewcommand{{\title}}{{{}}}'
-first_name: str = R'\newcommand{{\firstName}}{{{}}}'
-last_name: str = R'\newcommand{{\lastName}}{{{}}}'
-group: str = R'\newcommand{{\group}}{{{}}}'
-template: str = R'\input{{{}}}'
+        self.argument_parser = ArgumentParser(allow_abbrev = False, formatter_class = RawTextHelpFormatter,
+                                              description = self.description, epilog = self.additional_information, )
 
-clean: str = 'rm'
-clean_option: str = '-f'
-log_file: str = '{}.log'
-aux_file: str = '{}.aux'
+    def parse_arguments(self):
 
-if not arguments['non_interactive']:
-    template = template.format(join(join('faculties', input('Language: ')), input('Faculty: ') + '.tex'))
-    document_type = document_type.format(input('Type: '))
-    subject = subject.format(input('Subject: '))
-    temp_title = input('Title: ')
-    title = title.format(temp_title)
-    job_name = job_name.format(temp_title)
-    log_file = log_file.format(temp_title)
-    aux_file = aux_file.format(temp_title)
-    first_name = first_name.format(input('First name: '))
-    last_name = last_name.format(input('Last name: '))
-    group = group.format(input('Group: '))
+        self.argument_parser.add_argument('-ni', '--non-interactive', action = 'store_false',
+                                          help = 'Used if you want to pass the details in an non-interactive manner')
+        self.argument_parser.add_argument('-l', '--language', choices = ['RO', 'EN', 'FR', 'DE'], metavar = '',
+                                          type = str,
+                                          help = 'One of the four languages: RO/EN and FR/DE just for FILS students')
+        self.argument_parser.add_argument('-f', '--faculty', metavar = '', type = str,
+                                          help = 'The faculty at which you are a student')
+        self.argument_parser.add_argument('-t', '--type', metavar = '', type = str,
+                                          help = 'The document type (e.g. Project/Lab work/...)')
+        self.argument_parser.add_argument('-s', '--subject', metavar = '', type = str,
+                                          help = 'The university subject in question')
+        self.argument_parser.add_argument('-ti', '--title', metavar = '', type = str,
+                                          help = 'The title of the document')
+        self.argument_parser.add_argument('-fn', '--first-name', metavar = '', type = str, help = 'Your first name')
+        self.argument_parser.add_argument('-ln', '--last-name', metavar = '', type = str, help = 'Your last name')
+        self.argument_parser.add_argument('-g', '--group', metavar = '', type = str, help = 'The group you belong to')
+        self.arguments = vars(self.argument_parser.parse_args())
 
-else:
-    for elem in arguments:
-        if arguments[elem] is None:
-            argument_parser.error(f'Argument {elem} is empty. Every argument is required if -ni is not provided.')
-    template = template.format(join(join('faculties', arguments['language']), arguments['faculty'] + '.tex'))
-    document_type = document_type.format(arguments['type'])
-    subject = subject.format(arguments['subject'])
-    temp_title = arguments['title']
-    title = title.format(temp_title)
-    job_name = job_name.format(temp_title)
-    log_file = log_file.format(temp_title)
-    aux_file = aux_file.format(temp_title)
-    first_name = first_name.format(arguments['first_name'])
-    last_name = last_name.format(arguments['last_name'])
-    group = group.format(arguments['group'])
+    def process_arguments_or_input(self) -> Tuple[str, str, str, str, str, str, str, str]:
+        if self.arguments['non_interactive']:
+            for elem in self.arguments:
+                if self.arguments[elem] is None:
+                    self.argument_parser.error(
+                        f'Argument {elem} is empty. Every argument is required if -ni is not provided.')
+            return self.arguments['language'], self.arguments['faculty'], self.arguments['type'], self.arguments[
+                'subject'], self.arguments['title'], self.arguments['first_name'], self.arguments['last_name'], \
+                   self.arguments['group']
 
-information = ' '.join([document_type, subject, title, first_name, last_name, group, template])
-run([program, interaction, job_name, information])
-run([clean, clean_option, log_file, aux_file])
+        else:
+            return input('Language: '), input('Faculty: '), input('Type: '), input('Subject: '), input(
+                'Title: '), input('First Name: '), input('Last Name: '), input('Group: ')
+
+
+class PDFGenerator:
+
+    def __init__(self):
+        self.program: str = R'xelatex'
+        self.interaction: str = R'--interaction=scrollmode'
+        self.job_name: str = R'--jobname={}'
+
+        self.template: str = R'\input{{{}}}'
+        self.document_type: str = R'\newcommand{{\docType}}{{{}}}'
+        self.subject: str = R'\newcommand{{\subject}}{{{}}}'
+        self.title: str = R'\renewcommand{{\title}}{{{}}}'
+        self.first_name: str = R'\newcommand{{\firstName}}{{{}}}'
+        self.last_name: str = R'\newcommand{{\lastName}}{{{}}}'
+        self.group: str = R'\newcommand{{\group}}{{{}}}'
+        self.information: str = R''
+        self.name: str = R''
+
+        self.clean: str = R'rm'
+        self.clean_option: str = R'-f'
+        self.log_file: str = R'{}.log'
+        self.aux_file: str = R'{}.aux'
+
+    def get_options(self, options: Tuple[str, str, str, str, str, str, str, str]):
+        self.template = self.template.format(join(join('faculties', options[0]), options[1] + '.tex'))
+        self.document_type = self.document_type.format(options[2])
+        self.subject = self.subject.format(options[3])
+        self.title = self.title.format(options[4])
+        self.first_name = self.first_name.format(options[5])
+        self.last_name = self.last_name.format(options[6])
+        self.group = self.group.format(options[7])
+
+        self.information = ' '.join(
+            [self.document_type, self.subject, self.title, self.first_name, self.last_name, self.group, self.template])
+
+        self.job_name = self.job_name.format(options[4])
+        self.log_file = self.log_file.format(options[4])
+        self.aux_file = self.aux_file.format(options[4])
+        self.name = options[4]
+
+    def create_pdf(self) -> CompletedProcess:
+        return run([self.program, self.interaction, self.job_name, self.information], capture_output = True)
+
+    def clean_files(self) -> CompletedProcess:
+        return run([self.clean, self.clean_option, self.log_file, self.aux_file], capture_output = True)
+
+    def run(self):
+        class FileCreationError(Exception):
+            pass
+
+        create = self.create_pdf()
+        clean = self.clean_files()
+        if create.returncode == 0 and clean.returncode == 0:
+            print(F'PDF creation completed successfully. Output written to: {self.name}.pdf')
+        elif create.returncode != 0:
+            raise FileCreationError('PDF creation failed with code {create.returncode}.')
+        elif clean.returncode != 0:
+            print(F'PDF creation completed successfully. Output written to: {self.name}.pdf\n'
+                  F' Couldn\'t remove temporary files.')
+
+
+def main():
+    options_getter = OptionsGetter()
+    options_getter.parse_arguments()
+    pdf_generator = PDFGenerator()
+    pdf_generator.get_options(options_getter.process_arguments_or_input())
+    pdf_generator.run()
+
+
+if __name__ == '__main__':
+    main()
