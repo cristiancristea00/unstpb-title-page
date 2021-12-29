cde0b43c0b2ad3c8ab07f0385cb842233f4f9ae3
Cristian Cristea <cristiancristea00@gmail.com>
2021-12-29 22:46:15 +0200

    Updated process run

--- create.py
+++ create.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser, RawTextHelpFormatter
-from subprocess import CompletedProcess, run
-from os.path import join, exists
-from typing import Dict, Tuple
-from sys import stderr
 from os import remove
+from os.path import exists, join
+from subprocess import CalledProcessError, CompletedProcess, run
+from sys import stderr
+from typing import Dict, Tuple
 
 
 class OptionsGetter:
+
     def __init__(self):
         self.arguments: Dict[str] = {}
 
@@ -29,43 +30,40 @@ class OptionsGetter:
                                            'FSA - Faculty of Applied Sciences\n' \
                                            'FT - Faculty of Transports\n'
 
-        self.argument_parser = ArgumentParser(allow_abbrev = False, formatter_class = RawTextHelpFormatter,
-                                              description = self.description, epilog = self.additional_information, )
+        self.argument_parser = ArgumentParser(allow_abbrev=False, formatter_class=RawTextHelpFormatter,
+                                              description=self.description, epilog=self.additional_information, )
 
     def parse_arguments(self):
 
-        self.argument_parser.add_argument('-ni', '--non-interactive', action = 'store_false',
-                                          help = 'Used if you want to input the details in an non-interactive manner\n'
-                                                 'by passing all the information to command line arguments')
-        self.argument_parser.add_argument('-l', '--language', choices = ['RO', 'EN', 'FR', 'DE'], metavar = '',
-                                          type = str,
-                                          help = 'One of the four languages: RO/EN and FR/DE just for FILS students')
-        self.argument_parser.add_argument('-f', '--faculty', metavar = '', type = str,
-                                          help = 'The faculty at which you are a student (the initials provided below)')
-        self.argument_parser.add_argument('-t', '--type', metavar = '', type = str,
-                                          help = 'The document type (e.g. Project/Lab work/...)')
-        self.argument_parser.add_argument('-s', '--subject', metavar = '', type = str,
-                                          help = 'The university subject in question')
-        self.argument_parser.add_argument('-ti', '--title', metavar = '', type = str,
-                                          help = 'The title of the document')
-        self.argument_parser.add_argument('-fn', '--first-name', metavar = '', type = str, help = 'Your first name')
-        self.argument_parser.add_argument('-ln', '--last-name', metavar = '', type = str, help = 'Your last name')
-        self.argument_parser.add_argument('-g', '--group', metavar = '', type = str, help = 'The group you belong to')
+        self.argument_parser.add_argument('-ni', '--non-interactive', action='store_false',
+                                          help='Used if you want to input the details in an non-interactive manner\n'
+                                               'by passing all the information to command line arguments')
+        self.argument_parser.add_argument('-l', '--language', choices=['RO', 'EN', 'FR', 'DE'], metavar='',
+                                          type=str,
+                                          help='One of the four languages: RO/EN and FR/DE just for FILS students')
+        self.argument_parser.add_argument('-f', '--faculty', metavar='', type=str,
+                                          help='The faculty at which you are a student (the initials provided below)')
+        self.argument_parser.add_argument('-t', '--type', metavar='', type=str,
+                                          help='The document type (e.g. Project/Lab work/...)')
+        self.argument_parser.add_argument('-s', '--subject', metavar='', type=str,
+                                          help='The university subject in question')
+        self.argument_parser.add_argument('-ti', '--title', metavar='', type=str,
+                                          help='The title of the document')
+        self.argument_parser.add_argument('-fn', '--first-name', metavar='', type=str, help='Your first name')
+        self.argument_parser.add_argument('-ln', '--last-name', metavar='', type=str, help='Your last name')
+        self.argument_parser.add_argument('-g', '--group', metavar='', type=str, help='The group you belong to')
         self.arguments = vars(self.argument_parser.parse_args())
 
     def process_arguments_or_input(self) -> Tuple[str, str, str, str, str, str, str, str]:
         if not self.arguments['non_interactive']:
             for elem in self.arguments:
                 if self.arguments[elem] is None:
-                    self.argument_parser.error(
-                        f'Argument {elem} is empty. Every argument is required if -ni is provided.')
-            return self.arguments['language'], self.arguments['faculty'], self.arguments['type'], self.arguments[
-                'subject'], self.arguments['title'], self.arguments['first_name'], self.arguments['last_name'], \
-                   self.arguments['group']
+                    self.argument_parser.error(f'Argument {elem} is empty. Every argument is required if -ni is provided.')
+            return self.arguments['language'], self.arguments['faculty'], self.arguments['type'], self.arguments['subject'], \
+                   self.arguments['title'], self.arguments['first_name'], self.arguments['last_name'], self.arguments['group']
 
-        else:
-            return input('Language: '), input('Faculty: '), input('Type: '), input('Subject: '), input(
-                'Title: '), input('First Name: '), input('Last Name: '), input('Group: ')
+        return input('Language: '), input('Faculty: '), input('Type: '), input('Subject: '), input('Title: '), input('First Name: '), \
+               input('Last Name: '), input('Group: ')
 
 
 class PDFGenerator:
@@ -98,8 +96,7 @@ class PDFGenerator:
         self.last_name = self.last_name.format(options[6])
         self.group = self.group.format(options[7])
 
-        self.information = ' '.join(
-            [self.document_type, self.subject, self.title, self.first_name, self.last_name, self.group, self.template])
+        self.information = ' '.join([self.document_type, self.subject, self.title, self.first_name, self.last_name, self.group, self.template])
 
         self.job_name = self.job_name.format(options[4])
         self.log_file = self.log_file.format(options[4])
@@ -107,7 +104,7 @@ class PDFGenerator:
         self.name = options[4]
 
     def create_pdf(self) -> CompletedProcess:
-        return run([self.program, self.halt, self.interaction, self.job_name, self.information], capture_output = True)
+        return run([self.program, self.halt, self.interaction, self.job_name, self.information], capture_output=True, check=True)
 
     def clean_files(self):
         if exists(self.log_file):
@@ -116,20 +113,15 @@ class PDFGenerator:
             remove(self.aux_file)
 
     def run(self):
-        class FileCreationError(Exception):
-            pass
-
-        create = self.create_pdf()
-        if create.returncode == 0:
+        try:
+            self.create_pdf()
             print(F'PDF creation completed successfully. Output written to: {self.name}.pdf')
-        else:
-            print(F'PDF creation failed with exit code {create.returncode}. Maybe you provided an invalid language or '
-                  F'faculty. Check the output for more information.', '-' * 80, create.stdout.decode('UTF-8'), '-' * 80,
-                  sep = '\n', file = stderr)
+        except CalledProcessError as error:
+            print(F'PDF creation failed with exit code {error.returncode}. Maybe you provided an invalid language or '
+                  F'faculty. Check the output for more information.', '-' * 80, error.output, '-' * 80, sep='\n', file=stderr)
+            raise
+        finally:
             self.clean_files()
-            raise FileCreationError()
-
-        self.clean_files()
 
 
 def main():
